400fd7684cb43acb8005be48917472a07ccf611f
Alon Palgy <a.palgy@gmail.com>
2020-10-27 11:55:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+### Setup
+
+clone the repo
+
+    git clone this repo
+    cd into project folder
+    
+create a virtualenv and install packages
+    
+    python -m venv venv
+    pip install -r requiremnts.txt
+    
---
+++ books.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+# Don't touch this
+STATUS = 'Fail'
+TEXT_TO_FIND = 'placeat quia et porro iste'
+URLS = dict(
+    posts='https://jsonplaceholder.typicode.com/posts',
+    users='https://jsonplaceholder.typicode.com/users',
+)
+posts_response = json.loads(requests.get(URLS['posts']).content)
+users_response = json.loads(requests.get(URLS['users']).content)
+users = []
+
+
+class User:
+    def __init__(self, id, name, username, email, phone):
+        self.id = id
+        self.name = name
+        self.username = username
+        self.email = email
+        self.phone = phone
+        self.posts = []
+
+
+for user in users_response:
+    new_user = User(
+        id=user['id'],
+        name=user['name'],
+        username=user['username'],
+        email=user['email'],
+        phone=user['phone'],
+    )
+    for post in posts_response:
+        if post['userId'] == new_user.id:
+            new_user.posts.append(post)
+    users.append(new_user)
+
+
+# Dont touch this
+if users[8].posts[5]['title'] == TEXT_TO_FIND:
+    STATUS = 'Success'
+print(f'End result: {STATUS}')
+
+
+
